a536847f0dab7672036909df131af7047bb6c33b
Tray Berry <trayberry@comcast.net>
2024-03-16 00:33:34 -0400

    tooled around with spring animations more and took some notes for later reference. animations are still pretty bad though

--- client/src/components/TopBanner.js
+++ client/src/components/TopBanner.js
@@ -1,16 +1,17 @@
 import starLogo from '../images/kirbyStar.png'
 import { useSpring, animated, useChain, useSpringRef } from 'react-spring'
-import { useEffect } from 'react';
+import './styles/TopBanner.css'
 
 function TopBanner() {
     const starRef = useSpringRef();
     const textRef = useSpringRef();
     const moveUpRef = useSpringRef();
 
+    // use top, left, right, bottom, etc. as translate refers particularly to the element's own dimensions
     const starProps = useSpring({
         ref: starRef,
-        from: { transform: 'translateX(-100%)' },
-        to: { transform: 'translateX(0%)' },
+        from: { top: '50%', left: '-50%' },
+        to: { top: '50%', left: '50%' },
         config: { duration: 500 },
     })
 
@@ -25,16 +26,19 @@ function TopBanner() {
     const moveUpProps = useSpring({
         ref: moveUpRef,
         from: { transform: 'translateY(100%)' },
-        to: { transform: 'translateY(0%)' },
+        to: { transform: 'translateY(50%)' },
         config: { duration: 1000 },
+        delay:1000,
     })
 
     useChain([starRef, textRef, moveUpRef], [0, 0.5, 1.5]);
 
+    // Adding position: relative to the div and position: absolute to inside elements causes the absolute elements
+    // to reference to the most nearby relative element in terms of positioning
     return (
-        <animated.div style={moveUpProps}>
-            <animated.img src={starLogo} style={starProps} alt="" width="100px" />
-            <animated.p style={textProps}>TRAY!!!</animated.p>
+        <animated.div class='logoProps' style={{...moveUpProps, height:"100vh", width:"100vw", position: "relative"}}>
+            <animated.img src={starLogo} style={{...starProps, position:"absolute"}} alt="" width="100px" height="100px'" />
+            <animated.p style={{...textProps, position:"absolute"}}>TRAY!!!</animated.p>
         </animated.div>
     );
 }
